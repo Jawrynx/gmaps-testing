190f6401b945d65ee2b4ebbf9d74139acea4fec7
Jawrynx <jawrynx@gmail.com>
2024-09-01 11:15:30 +0100

    Added a second map

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-// Initialize and add the map
-let map;
+// Initialize and add the maps
+let fromMap, toMap;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  // Locations
+  const fromPosition = { lat: -25.344, lng: 131.031 }; // Uluru
+  const toPosition = { lat: 51.5074, lng: 0.1278 }; // London (example)
+
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
+  // The fromMap, centered at fromPosition
+  fromMap = new Map(document.getElementById("fromMap"), {
     zoom: 4,
-    center: position,
+    center: fromPosition,
     mapId: "DEMO_MAP_ID",
   });
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
+  // The toMap, centered at toPosition
+  toMap = new Map(document.getElementById("toMap"), {
+    zoom: 4,
+    center: toPosition,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // Markers
+  new AdvancedMarkerElement({
+    map: fromMap,
+    position: fromPosition,
     title: "Uluru",
+  });
+
+  new AdvancedMarkerElement({
+    map: toMap,
+    position: toPosition,
+    title: "London",
   });
 }
 
